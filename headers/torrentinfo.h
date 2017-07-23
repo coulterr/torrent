54c2ac4f6d04b02564765c7831ec395c88e096ff
Riley Coulter <rcoulter35@gmail.com>
2017-07-23 15:36:44 -0400

    Added struct members fileinfo and segmentinfo to torrentinfo

--- headers/torrentinfo.h
+++ headers/torrentinfo.h
@@ -2,16 +2,22 @@
 #define TORRENTINFO_H
 
 #include <stdlib.h>
-
+#include <string.h>
+#include "arraylist.h"
+#include "fileinfo.h"
+#include "segmentinfo.h"
 
 typedef struct {
 
-	char meta_path[1024]; 
+	char metapath[1024]; 
+	char dirpath[1024];
 	char downloaded_segments[250000];
+	Arraylist *files;
+	Arraylist *segments; 
 
 }Torrentinfo; 
 
-int Torrentinfo_init(Torrentinfo **info);
+int Torrentinfo_init(Torrentinfo **info, const char *metapath, const char *dirpath);
 int Torrentinfo_delete(Torrentinfo *info);
 
 #endif
--- torrentinfo.c
+++ torrentinfo.c
@@ -1,14 +1,20 @@
 #include "headers/torrentinfo.h"
 
 
-int Torrentinfo_init(Torrentinfo **info)
+int Torrentinfo_init(Torrentinfo **info, const char *metapath, const char *dirpath)
 {
-	*info = malloc(sizeof(Torrentinfo); 
+	*info = malloc(sizeof(Torrentinfo)); 
+	Arraylist_init(&((*info)->files)); 
+	Arraylist_init(&((*info)->segments)); 
+	strcpy((*info)->metapath, metapath); 
+	strcpy((*info)->dirpath, dirpath); 
 
 }
 
 
 int Torrentinfo_delete(Torrentinfo *info)
 {
+	Arraylist_delete(info->files, Fileinfo_delete); 
+	Arraylist_delete(info->files, Segmentinfo_delete);
 	free(info); 
 }
