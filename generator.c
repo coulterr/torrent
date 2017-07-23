97bf69a0968921f43bf47a1edee4d9b527b9d4f4
Riley Coulter <rcoulter35@gmail.com>
2017-07-23 14:21:54 -0400

    Generator now overwrites meta file if it exists

--- generator.c
+++ generator.c
@@ -11,17 +11,16 @@
 #include "headers/bin_to_hex.h"
 #include "headers/charlist.h"
 
-char *generate_file_metadata(const char *src_path, const char *meta_path, const size_t block_size)
+char *generate_file_metadata(const char *src_path, FILE *metafile, const size_t block_size)
 {	
 	size_t hex_length = SHA_DIGEST_LENGTH * 2; 
 	FILE *source = fopen(src_path, "r"); 
-	FILE *dest = fopen(meta_path, "a"); 
 	unsigned char buff[block_size]; 
 	unsigned char hash[SHA_DIGEST_LENGTH]; 
 	unsigned char hex[hex_length]; 
 
-	fwrite(src_path, sizeof(char), strlen(src_path), dest); 
-	fwrite("\n", sizeof(char), 1, dest); 
+	fwrite(src_path, sizeof(char), strlen(src_path), metafile); 
+	fwrite("\n", sizeof(char), 1, metafile); 
 
 	Charlist *charlist; 
 	Charlist_init(&charlist); 
@@ -37,23 +36,23 @@ char *generate_file_metadata(const char *src_path, const char *meta_path, const 
 		Charlist_add(charlist, hex, hex_length); 
 	}
 	
-	fprintf(dest, "%i\n", sum); 
+	fprintf(metafile, "%i\n", sum); 
 	
 	for (size_t i = 0; i < (sum + (block_size - 1)) / block_size; i++)
 	{
-		fwrite((charlist -> data) + hex_length * i, sizeof(char), hex_length, dest); 
+		fwrite((charlist -> data) + hex_length * i, sizeof(char), hex_length, metafile); 
 	}
-	fwrite("\n", sizeof(char), 1, dest); 
+	fwrite("\n", sizeof(char), 1, metafile); 
 
 	Charlist_clean(charlist); 
 	free(charlist); 
 
 	fclose(source); 
-	fclose(dest); 
+	fclose(metafile); 
 }
 
 
-void generate_metadata(const char *src_dir, const char *meta_path, const size_t block_size)
+void generate_metadata(const char *src_dir, FILE *metafile, const size_t block_size)
 {
 	DIR *dir; 
 	struct dirent *entry; 
@@ -61,10 +60,16 @@ void generate_metadata(const char *src_dir, const char *meta_path, const size_t 
 	while ((entry = readdir(dir)) != NULL) {
 		char path[1024]; 
 		snprintf(path, sizeof(path), "%s/%s", src_dir, entry->d_name); 
-		if (entry->d_type == DT_REG) generate_file_metadata((const char *) path, meta_path, block_size); 
+		if (entry->d_type == DT_REG) {
+			generate_file_metadata((const char *) path, metafile, block_size); 
+		}
 		else if (entry->d_type == DT_DIR) {
-			if (strcmp(entry->d_name , ".") == 0 || strcmp(entry->d_name, "..") == 0) continue; 
-			generate_metadata((const char *) path, meta_path, block_size); 
+			if (strcmp(entry->d_name , ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+				continue; 
+			}
+			else {
+				generate_metadata((const char *) path, metafile, block_size); 
+			}
 		}
 	}
 	closedir(dir); 
@@ -77,5 +82,10 @@ int main (int argc, char **argv)
 	const char *src_dir = argv[1]; 
 	const char *meta_path = argv[2]; 
 
-	generate_metadata(src_dir, meta_path, block_size); 	
+	FILE *metafile = fopen(meta_path, "w"); 
+
+	generate_metadata(src_dir, metafile, block_size);
+
+	fclose(metafile); 
+
 }
