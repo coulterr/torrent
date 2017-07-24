639072f4964df64e6b65848c4b6055a4274d0ca7
Riley Coulter <rcoulter35@gmail.com>
2017-07-24 15:09:52 -0400

    Moved fork logic to start_daemon() function

--- client.c
+++ client.c
@@ -6,25 +6,28 @@
 
 int start_daemon(char *map_path)
 {
-	Arraylist *torrents; 
-	Arraylist_init(&torrents); 
+	signal(SIGCHLD, SIG_IGN); 
 
-	get_meta_info(torrents, map_path); 
-	for(size_t i = 0; i < torrents->size; i++){
-		print_info(Arraylist_get(torrents, i)); 
+	if (!fork()) {
+	
+		Arraylist *torrents; 
+		Arraylist_init(&torrents); 
+
+		get_meta_info(torrents, map_path); 
+		
+		for(size_t i = 0; i < torrents->size; i++){
+			print_info(Arraylist_get(torrents, i)); 
+		}
+
+		Arraylist_delete(torrents, &Torrentinfo_delete); 
 	}
-
-	Arraylist_delete(torrents, &Torrentinfo_delete); 
 }
 
 
 int main(int argc, char **argv)
 {
-	signal(SIGCHLD, SIG_IGN); 
-
 	if (strcmp(argv[1], "start") == 0) {
-		if (!fork()) {
-			start_daemon(argv[2]); 
-		}
+		start_daemon(argv[2]);
 	}
+
 }
