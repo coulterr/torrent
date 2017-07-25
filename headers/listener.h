b8e9b87e62ad5e35f05d2eafdeec8f5ef571649e
Riley Coulter <rcoulter35@gmail.com>
2017-07-25 12:19:18 -0400

    Started server-side Unix socket logic

--- headers/listener.h
+++ headers/listener.h
@@ -3,6 +3,11 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
 #include "arraylist.h"
 
 void *start_listening(void *arg);
--- listener.c
+++ listener.c
@@ -6,4 +6,33 @@ void *start_listening(void *arg)
 
 	printf("Thread started...\n"); 
 
+	unsigned int s, s2; 
+	struct sockaddr_un local, remote; 
+	int len; 
+
+	s = socket(AF_UNIX, SOCK_STREAM, 0); 
+	local.sun_family = AF_UNIX; 
+	strcpy(local.sun_path, "./communication"); 
+	unlink(local.sun_path); 
+	len = strlen(local.sun_path) + sizeof(local.sun_family); 
+	bind(s, (struct sockaddr *) &local, len); 
+
+	listen(s, 5); 
+
+	len = sizeof(struct sockaddr_un); 
+	char buff[1024]; 
+	
+	while (s2 = accept(s, (struct sockaddr *) &remote, &len)) {
+		len = recv(s2, buff, 100, 0); 
+		buff[len] = '\0'; 
+		if(strcmp(buff, "stop") == 0){
+			close(s2); 
+			break;
+		}
+		send(s2, buff, len, 0); 
+		close(s2); 
+	}
+
+	close(s); 
+
 }
